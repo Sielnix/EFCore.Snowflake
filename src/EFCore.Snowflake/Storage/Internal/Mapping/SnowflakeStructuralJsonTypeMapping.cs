0d83db0598a7629ccc0b580b5bd2615474e6a5fa
Sielnix <krzysztof.sielaff@gmail.com>
2026-05-27 18:59:00 +0200

    Structural json extensions

--- src/EFCore.Snowflake/Storage/Internal/Mapping/SnowflakeStructuralJsonTypeMapping.cs
+++ src/EFCore.Snowflake/Storage/Internal/Mapping/SnowflakeStructuralJsonTypeMapping.cs
@@ -1,3 +1,7 @@
+using System.Data.Common;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
 using Microsoft.EntityFrameworkCore.Storage;
 
 namespace EFCore.Snowflake.Storage.Internal.Mapping;
@@ -5,6 +9,19 @@ namespace EFCore.Snowflake.Storage.Internal.Mapping;
 public class SnowflakeStructuralJsonTypeMapping : SnowflakeSemiStructuredTypeMapping
 {
     public new static readonly SnowflakeStructuralJsonTypeMapping Default = new();
+
+    private static readonly MethodInfo GetStringMethod
+        = typeof(DbDataReader).GetRuntimeMethod(nameof(DbDataReader.GetString), [typeof(int)])!;
+
+    private static readonly PropertyInfo UTF8Property
+        = typeof(Encoding).GetProperty(nameof(Encoding.UTF8))!;
+
+    private static readonly MethodInfo EncodingGetBytesMethod
+        = typeof(Encoding).GetMethod(nameof(Encoding.GetBytes), [typeof(string)])!;
+
+    private static readonly ConstructorInfo MemoryStreamConstructor
+        = typeof(MemoryStream).GetConstructor([typeof(byte[])])!;
+    
     public SnowflakeStructuralJsonTypeMapping()
         : base(SnowflakeStoreTypeNames.Variant, typeof(JsonTypePlaceholder))
     {
@@ -19,4 +36,21 @@ public class SnowflakeStructuralJsonTypeMapping : SnowflakeSemiStructuredTypeMap
     {
         return new SnowflakeStructuralJsonTypeMapping(parameters);
     }
+
+    public override MethodInfo GetDataReaderMethod()
+        => GetStringMethod;
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public override Expression CustomizeDataReaderExpression(Expression expression)
+        => Expression.New(
+            MemoryStreamConstructor,
+            Expression.Call(
+                Expression.Property(null, UTF8Property),
+                EncodingGetBytesMethod,
+                expression));
 }
--- test/EFCore.Snowflake.FunctionalTests/ComplexTypesTrackingSnowflakeTest.cs
+++ test/EFCore.Snowflake.FunctionalTests/ComplexTypesTrackingSnowflakeTest.cs
@@ -13,6 +13,19 @@ public class ComplexTypesTrackingSnowflakeTest : ComplexTypesTrackingRelationalT
     {
     }
 
+    // Custom JSON query translation is not implemented
+    public override Task Can_change_state_from_Deleted_with_complex_collection(EntityState newState, bool async)
+        => Assert.ThrowsAsync<InvalidOperationException>(() => base.Can_change_state_from_Deleted_with_complex_collection(newState, async));
+
+    public override Task Can_change_state_from_Deleted_with_complex_field_collection(EntityState newState, bool async)
+        => Assert.ThrowsAsync<InvalidOperationException>(() => base.Can_change_state_from_Deleted_with_complex_field_collection(newState, async));
+
+    public override Task Can_change_state_from_Deleted_with_complex_field_record_collection(EntityState newState, bool async)
+        => Assert.ThrowsAsync<InvalidOperationException>(() => base.Can_change_state_from_Deleted_with_complex_field_record_collection(newState, async));
+
+    public override Task Can_change_state_from_Deleted_with_complex_record_collection(EntityState newState, bool async)
+        => Assert.ThrowsAsync<InvalidOperationException>(() => base.Can_change_state_from_Deleted_with_complex_record_collection(newState, async));
+
     protected override void UseTransaction(DatabaseFacade facade, IDbContextTransaction transaction)
         => facade.UseTransaction(transaction.GetDbTransaction());
 
